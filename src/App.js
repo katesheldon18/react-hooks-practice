d5c5a7741169e8cc88c9b882d945472b35fdfc6f
Kate <katshe18@gmail.com>
2024-01-22 14:07:57 +1300

    Added a Color Picker using 'useState'

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
+  //Practising useState to make a Counter and a Color Picker:
+
   useState(0);
 
   const [count, setCount] = useState(0);
+  const [color, changeColor] = useState("black");
 
   let increase = () => {
     setCount(count + 1);
@@ -16,10 +19,19 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1>Kate's Counter</h1>
-      <button onClick={increase}>+</button>
-      <div>{count}</div>
-      <button onClick={decrease}>-</button>
+      <div className="component-container">
+        <h1>Kate's Counter</h1>
+        <button onClick={increase}>+</button>
+        <div>{count}</div>
+        <button onClick={decrease}>-</button>
+      </div>
+
+      <div className="component-container">
+        <h1 style={{ color }}>Kate's Color Picker</h1>
+        <button onClick={() => changeColor("green")}>Green</button>
+        <button onClick={() => changeColor("red")}>Red</button>
+        <button onClick={() => changeColor("blue")}>Blue</button>
+      </div>
     </div>
   );
 };
